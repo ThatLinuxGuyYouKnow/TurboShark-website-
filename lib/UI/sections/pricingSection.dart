c996a9b099205719d4e9e5af529c80a427e985b7
Emmanuels-code <arocket04@gmail.com>
2024-12-16 09:16:16 +0100

    chore: modified pricing section

---
+++ lib/UI/sections/pricingSection.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PricingSection extends StatelessWidget {
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Container(
+      child: Padding(padding: EdgeInsets.only(top: 100)),
+    );
+  }
+}
