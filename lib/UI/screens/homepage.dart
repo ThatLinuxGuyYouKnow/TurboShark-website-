a66e1f9cc6c5c7e9562f15b853522a5a58199699
Emmanuels-code <arocket04@gmail.com>
2024-12-16 09:25:41 +0100

    chore: moved pricing text into pricing section

--- lib/UI/screens/homepage.dart
+++ lib/UI/screens/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:turbo_shark_website/UI/sections/featureSection.dart';
 import 'package:turbo_shark_website/UI/sections/heroSection.dart';
+import 'package:turbo_shark_website/UI/sections/pricingSection.dart';
 import 'package:turbo_shark_website/UI/widgets/appBars.dart';
 
 class Homepage extends StatelessWidget {
@@ -27,11 +28,7 @@ class Homepage extends StatelessWidget {
           SizedBox(
             height: 100,
           ),
-          Text(
-            'Prcing Plans',
-            style: GoogleFonts.ubuntu(
-                fontSize: 40, fontWeight: FontWeight.bold, color: Colors.blue),
-          )
+          PricingSection()
         ]),
       ),
     );
--- lib/UI/sections/pricingSection.dart
+++ lib/UI/sections/pricingSection.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PricingSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
     return Container(
-      child: Padding(padding: EdgeInsets.only(top: 100)),
+      width: screenWidth,
+      color: Colors.blue.shade300.withOpacity(0.1),
+      child: Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Column(
+          children: [
+            Text(
+              'Prcing Plans',
+              style: GoogleFonts.ubuntu(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
