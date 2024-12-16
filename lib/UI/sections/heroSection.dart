5f2a4bd304013e24f65a4791f76af1d717069aaf
Emmanuels-code <arocket04@gmail.com>
2024-12-16 23:23:45 +0100

    feature: improved genral responsiveness and spacing

--- lib/UI/sections/heroSection.dart
+++ lib/UI/sections/heroSection.dart
@@ -6,27 +6,27 @@ class HeroSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
-    bool smallScreen = screenWidth < 600;
-    final bool smallscreenHeight = MediaQuery.of(context).size.height < 900;
+    final bool smallScreen = screenWidth < 600;
+    final bool smallScreenHeight = MediaQuery.of(context).size.height < 900;
+
     return Container(
-      height: smallScreen ? 300 : 600,
+      height: smallScreen ? 400 : 600,
       width: screenWidth,
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: [Colors.blue.shade300, Colors.blue.shade700]),
+          colors: [Colors.blue.shade300, Colors.blue.shade700],
+        ),
       ),
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
-            SizedBox(
-              height: smallscreenHeight ? 80 : 160,
-            ),
+            SizedBox(height: smallScreenHeight ? 60 : 120),
             Text(
               'Turbo Shark',
               style: GoogleFonts.russoOne(
                 color: Colors.white,
-                fontSize: smallScreen ? 28 : 35,
+                fontSize: smallScreen ? 28 : 40,
                 fontWeight: FontWeight.bold,
                 letterSpacing: 3,
               ),
@@ -36,7 +36,7 @@ class HeroSection extends StatelessWidget {
               'Download FASTER than ever!',
               textAlign: TextAlign.center,
               style: GoogleFonts.ubuntu(
-                fontSize: smallScreen ? 24 : 28,
+                fontSize: smallScreen ? 20 : 28,
                 letterSpacing: smallScreen ? 0.6 : 1.5,
                 color: Colors.white,
                 fontWeight: FontWeight.w600,
@@ -44,11 +44,11 @@ class HeroSection extends StatelessWidget {
             ),
             const SizedBox(height: 15),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
                 'Swim circles around slow downloads. The fastest download experience*, completely free.',
                 style: GoogleFonts.ubuntu(
-                  fontSize: smallScreen ? 20 : 22,
+                  fontSize: smallScreen ? 16 : 20,
                   letterSpacing: 0,
                   color: Colors.white,
                   fontWeight: FontWeight.w400,
@@ -56,25 +56,38 @@ class HeroSection extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
             ),
-            const SizedBox(height: 50),
-            const Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 700), //TODO: Make responsive
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  HeroButtons(
-                      icon: Icon(
-                        Icons.download,
-                      ),
-                      buttonText: 'Download'),
-                  HeroButtons(
-                    buttonText: 'Learn More',
-                    icon: Icon(Icons.read_more),
-                  )
-                ],
-              ),
-            )
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: smallScreen
+                  ? const Column(
+                      children: [
+                        HeroButtons(
+                          icon: Icon(Icons.download, color: Colors.white),
+                          buttonText: 'Download',
+                        ),
+                        SizedBox(height: 10),
+                        HeroButtons(
+                          icon: Icon(Icons.read_more, color: Colors.white),
+                          buttonText: 'Learn More',
+                        ),
+                      ],
+                    )
+                  : const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        HeroButtons(
+                          icon: Icon(Icons.download, color: Colors.white),
+                          buttonText: 'Download',
+                        ),
+                        SizedBox(width: 20),
+                        HeroButtons(
+                          icon: Icon(Icons.read_more, color: Colors.white),
+                          buttonText: 'Learn More',
+                        ),
+                      ],
+                    ),
+            ),
           ],
         ),
       ),
